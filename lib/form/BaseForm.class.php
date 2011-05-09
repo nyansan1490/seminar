7a3ddb8a2f05be6e1efd9df97224c5204b61ef64
Hitomi Shida <nyansan1490@gmail.com>
2011-05-09 17:03:44 +0900

    symfony setuped

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    seminar
+ * @subpackage form
+ * @author     Your name here 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
